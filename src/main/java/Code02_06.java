9a5516742e5fbd42021fe1fe827593931887cc9d
mikuminu <minwoo.miku@gmail.com>
2026-03-18 09:37:36 +0900

    변수 선언

---
+++ src/main/java/Code02_06.java
@@ -0,0 +1,10 @@
+public class Code02_06 {
+    public static void main(String[] args) {
+        int num1, num2, sum;
+        num1 = 100;
+        num2 = 50;
+        sum = num1 + num2;
+
+        System.out.println(num1 + "+" + num2 + "=" + sum);
+    }
+}
